af0c4504046c1e180f73f88437f07e017d5047b4
RyanLang44 <ryanlang44@gmail.com>
2019-02-12 10:52:40 +0000

    commit

--- AccountSETemplate/src/main/java/com/qa/persistence/repository/AccountMapRepository.java
+++ AccountSETemplate/src/main/java/com/qa/persistence/repository/AccountMapRepository.java
@@ -2,10 +2,16 @@ package com.qa.persistence.repository;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import com.qa.persistence.domain.Account;
+import com.qa.util.JSONUtil;
+import com.qa.util.JackSonUtil;
 
 public class AccountMapRepository implements AccountRepository{
+	
+	JackSonUtil util = new JackSonUtil();
+	private static long id;
 	
 	Map<Long, Account> accountMap = new HashMap<Long, Account>();
 	
@@ -13,23 +19,26 @@ public class AccountMapRepository implements AccountRepository{
 	//do not change the method signature
 
 	public String getAllAccounts() {
-		// TODO Auto-generated method stub
-		return null;
+		String str = "";
+		for (Entry<Long, Account> entry: accountMap.entrySet()) {
+			str += util.getJSONFromObject(entry.getValue());
+		}
+		return str;
 	}
 
 	public String createAccount(String account) {
-		// TODO Auto-generated method stub
-		return null;
+		this.accountMap.put(id++, util.getObjectFromJSON(account, Account.class));
+		return account;
 	}
 
 	public String deleteAccount(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		this.accountMap.remove(id);
+		return "Account has been removed.";
 	}
 
 	public String updateAccount(Long id, String account) {
-		// TODO Auto-generated method stub
-		return null;
+		this.accountMap.replace(id, util.getObjectFromJSON(account, Account.class));
+		return "Account updated.";
 	}
 
 }
